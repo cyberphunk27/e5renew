747ba68b3ba351cdb31e1920ee5532ac754e8e6b
cyberphunk27 <63706750+cyberphunk27@users.noreply.github.com>
2024-11-04 14:00:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ calls = [
     'https://api.powerbi.com/v1.0/myorg/apps',
     'https://graph.microsoft.com/v1.0/me/mailFolders',
     'https://graph.microsoft.com/v1.0/applications?$count=true',
-        'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',
+    'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',
     'https://graph.microsoft.com/v1.0/me/?$select=displayName,skills',
     'https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta',
     'https://graph.microsoft.com/beta/me/outlook/masterCategories',
